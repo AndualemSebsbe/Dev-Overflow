85465cf33dd7a8865e55edb1cff1f259e514a68c
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2023-10-08 22:19:46 +0300

    fix: _id data type

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { Badge } from '../ui/badge'
 
 interface RenderTagProps {
-    _id: string
+    _id: number
     name: string
     totalQuestions?: number
     showCount?: boolean
@@ -13,7 +13,7 @@ function RenderTag({_id, name, totalQuestions, showCount}: RenderTagProps) {
   return (
     <Link href={`tags/${_id}`} key={_id} className='flex justify-between gap-2'>
       <Badge className='subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase' >{name}</Badge>
-      
+
       {showCount && (
         <p className='small-medium text-dark500_light700'>{totalQuestions}</p>
       )
