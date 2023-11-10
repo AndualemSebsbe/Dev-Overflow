afb4354ad8ca4b1edef628bb494869e8ff77f8f5
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2023-11-10 05:23:28 +0300

    feat: implement question details

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -4,7 +4,7 @@ import Question from "@/database/question.model";
 import { connectToDatabase } from "../moongose"
 import Tag from "@/database/tag.model";
 import { revalidatePath } from "next/cache";
-import { CreateQuestionParams, GetQuestionsParams } from "./shared";
+import { CreateQuestionParams, GetQuestionByIdParams, GetQuestionsParams } from "./shared";
 import User from "@/database/user.model";
 
 export async function getQuestions(params: GetQuestionsParams) {
@@ -59,3 +59,20 @@ export async function createQuestion(params: CreateQuestionParams) {
         
     }
 }
+
+export async function getQuestionById(params: GetQuestionByIdParams) {
+    try {
+      connectToDatabase();
+  
+      const { questionId } = params;
+  
+      const question = await Question.findById(questionId)
+        .populate({ path: 'tags', model: Tag, select: '_id name'})
+        .populate({ path: 'author', model: User, select: '_id clerkId name picture'})
+  
+        return question;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
