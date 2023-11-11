d35dd1e4ced92588835dcc79e34b4fde538b91dd
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2023-11-11 03:41:12 +0300

    feat: implement filtering

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -8,40 +8,56 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { formUrlQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
 
-interface FilterProps {
-    filters: {
-        name: string
-        value: string
-    } []
-    otherClasses?: string
-    containerClasses?: string
+interface Props {
+  filters: {
+    name: string,
+    value: string,
+  }[];
+  otherClasses?: string;
+  containerClasses?: string;
 }
-function Filter({
-    filters,
-    otherClasses,
-    containerClasses
-}: FilterProps) {
+
+const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const paramFilter = searchParams.get('filter');
+
+  const handleUpdateParams = (value: string) => {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: 'filter',
+      value
+    })
+
+    router.push(newUrl, { scroll: false })
+  }
+
   return (
     <div className={`relative ${containerClasses}`}>
-        <Select>
-            <SelectTrigger className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}>
-            <div className="line-clamp-1 flex-1 text-left">
-                <SelectValue placeholder="Select a Filter" />
-            </div>    
-            </SelectTrigger>
-            <SelectContent>
-                <SelectGroup>
-                    {filters.map((item) => (
-                        <SelectItem key={item.value} value={item.value} className="light-border background-light800_dark300 text-dark500_light700">
-                            {item.name}
-                        </SelectItem>
-                    ))}
-                </SelectGroup>
-            </SelectContent>
-        </Select>
+      <Select
+        onValueChange={handleUpdateParams}
+        defaultValue={paramFilter || undefined}
+      >
+        <SelectTrigger className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}>
+        <div className="line-clamp-1 flex-1 text-left">
+          <SelectValue placeholder="Select a Filter" />
+        </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {filters.map((item) => (
+              <SelectItem key={item.value} value={item.value}>
+                {item.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </div>
-
   )
 }
 
